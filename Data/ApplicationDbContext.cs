93d45832dc3c3e17ece931f2ff836b4733781405
Merthynz <rerive@gmail.com>
2022-10-02 12:18:31 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AddressBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace AddressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
